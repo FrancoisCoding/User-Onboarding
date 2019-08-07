4e3d500945b3788f39e33f1704aa012e5712c948
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-07 12:26:56 -0400

    Added custom error messages

--- user-onboarding/src/components/TheForm.js
+++ user-onboarding/src/components/TheForm.js
@@ -2,19 +2,22 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const TheForm = ({ values, handleChange }) => (
+const TheForm = ({ values, errors, touched, isSubmitting }) => (
   <Form>
     <label>
       Name
+      {touched.name && errors.name && <p>{errors.name}</p>}
       <Field type="text" name="name" placeholder="Full Name" />
     </label>
     <label>
       Email
+      {touched.email && errors.email && <p>{errors.email}</p>}
       <Field type="text" name="email" placeholder="Email" />
     </label>
     <label>
       Password
-      <Field type="text" name="password" placeholder="Password" />
+      {touched.password && errors.password && <p>{errors.password}</p>}
+      <Field type="password" name="password" placeholder="Password" />
     </label>
     <label>
       Terms of Service
@@ -35,14 +38,14 @@ const FormikForm = withFormik({
   },
   validationSchema: Yup.object().shape({
     name: Yup.string()
-      .min(6)
-      .required(),
+      .min(6, "Please Provide Your First and Last Name")
+      .required("Please Provide Your Name"),
     email: Yup.string()
       .email()
-      .required(),
+      .required("Please Provide Your Email"),
     password: Yup.string()
-      .min(9)
-      .required()
+      .min(9, "Password must be at least 9 characters")
+      .required("Please Provide Your Password")
   }),
   handleSubmit(values) {
     console.log(values);
