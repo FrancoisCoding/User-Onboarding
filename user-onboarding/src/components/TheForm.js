06fc8d96cfd3d2930930f6913c67689a9b548b6d
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-07 15:21:56 -0400

    Finished styling and stretch assignments

--- user-onboarding/src/components/TheForm.js
+++ user-onboarding/src/components/TheForm.js
@@ -1,47 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function TheForm({ values, errors, touched, isSubmitting }) {
-  console.log("users", values.users);
+function TheForm({ values, errors, touched, isSubmitting, status, form }) {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    // status sometimes comes through as undefined
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
   return (
     <>
       <Form>
         <label>
           Name
-          {touched.name && errors.name && <p>{errors.name}</p>}
+          {touched.name && errors.name && (
+            <p className="errors">{errors.name}</p>
+          )}
           <Field type="text" name="name" placeholder="Full Name" />
         </label>
         <label>
           Email
-          {touched.email && errors.email && <p>{errors.email}</p>}
+          {touched.email && errors.email && (
+            <p className="errors">{errors.email}</p>
+          )}
           <Field type="text" name="email" placeholder="Email" />
         </label>
         <label>
           Password
-          {touched.password && errors.password && <p>{errors.password}</p>}
+          {touched.password && errors.password && (
+            <p className="errors">{errors.password}</p>
+          )}
           <Field type="password" name="password" placeholder="Password" />
+        </label>
+        <label>
+          Choose Role
+          <Field component="select" name="role" className="selectOption">
+            <option value="noob">Noob</option>
+            <option value="basic">Basic</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="pro">Pro</option>
+          </Field>
         </label>
         <label>
           Terms of Service
           <Field type="checkbox" name="terms" checked={values.terms} />
         </label>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </Form>
       <h1>Current Users</h1>
-      {values.user && values.users.map(user => console.log("each user", user))}
+      {users
+        ? users.map(user => (
+            <p key={user.id} className="users">
+              Name : {user.name} Role : {user.role}
+            </p>
+          ))
+        : null}
     </>
   );
 }
 
 const FormikForm = withFormik({
-  mapPropsToValues({ name, email, password, terms, users }) {
+  //Here  remember to handle the inputs using the args you give to you mapPropsToValues
+
+  enableReinitialize: true,
+  mapPropsToValues: ({ name, email, password, terms, users, role }) => {
     return {
       name: "",
       email: "",
       password: "",
       terms: false,
+      role: "noob",
       users: ["Example User"]
     };
   },
@@ -56,19 +87,17 @@ const FormikForm = withFormik({
       .min(9, "Password must be at least 9 characters")
       .required("Please Provide Your Password")
   }),
-  handleSubmit(values, { resetForm }) {
-    console.log(values);
+  handleSubmit(values, { resetForm, setStatus }) {
     axios
       .post("https://reqres.in/api/users", {
         name: values.name,
         email: values.email,
         password: values.password,
-        terms: values.terms
+        terms: values.terms,
+        role: values.role
       })
       .then(response => {
-        console.log(response);
-        values.users = [...values.users, response.data];
-        console.log(values.users);
+        setStatus(response.data);
       })
       .catch(function(error) {
         console.log(error);
